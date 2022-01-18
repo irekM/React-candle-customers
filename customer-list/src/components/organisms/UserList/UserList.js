e8a5899b8844692115cb3b5841e154c386f8de1e
Your Name <you@example.com>
2022-01-18 21:40:41 +0100

    change components for hooks

--- customer-list/src/components/organisms/UserList/UserList.js
+++ customer-list/src/components/organisms/UserList/UserList.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { users } from 'data/users';
+import React, {useState, useEffect} from 'react';
+import { users  as usersData} from 'data/users';
 import UserListItem from 'components/molecules/UserListItem/UserListItem';
 import { StyledList, Wrapper } from './UserList.styles';
 
@@ -7,8 +7,8 @@ import { StyledList, Wrapper } from './UserList.styles';
 const mockAPI = (success) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if(success) {
-                resolve([...users]);
+            if(usersData) {
+                resolve([...usersData]);
             } else {
                 reject({message: 'Error'})
             }
@@ -16,47 +16,41 @@ const mockAPI = (success) => {
     })
 }
 
+const UsersList = () => {
 
-class UserList extends React.Component {
-    state = {
-       users: [],
-    }
+    const [users, setUsers] = useState([]);
+    const [isLoading, setLoadingState] = useState([]);
 
-    componentDidMount() {
-       this.setState({isLoading: true});
-       mockAPI().then(date => {
-           this.setState({isLoading: false});
-           this.setState({users: data});
-       })
-    .catch(err =>console.log(err));
-    }
 
-    componentDidUpdate(prevState){
-        if(prevState.isLoading !== this.state.isLoading){
-            console.log('Wczytywany stan zostal zmieniony');
-        }
-    }
+    useEffect( () => {
+        setLoadingState(true);
+        mockAPI().then(data => {
+            setLoadingState(false);
+            setUsers(data)
+            })
+            .catch(err =>console.log(err));
+    },[])
 
-    deleteUser = (imie) => {
-        const filteredUsers = this.state.users.filter(user => user.imie !== imie);
-        this.setState({users:filteredUsers});
-    }
 
-    render(){
-        return(
+   const deleteUser = (imie) => {
+        const filteredUsers =users.filter(user => user.imie !== imie);
+        setUsers(filteredUsers)
+    };
+
+
+
+    return(
             
-                <Wrapper>
-                    <h1>{this.state.isLoading ? 'Loading...' : 'Users list'}</h1>
-                    <StyledList>
-                        {this.state.users.map((userData) => (
-                            <UserListItem key={userData.imie} userData={userData}
-                userData={userData}/>
-                        ))}
-                    </StyledList>
-                </Wrapper>
-                        
-        );
-    }
+        <Wrapper>
+            <h1>{isLoading ? 'Loading...' : 'Users list'}</h1>
+            <StyledList>
+                {users.map((userData) => (
+                    <UserListItem deleteUser={deleteUser}key={userData.imie} userData={userData}/>
+                ))}
+            </StyledList>
+        </Wrapper>
+                
+);
 }
 
 export default UserList;
