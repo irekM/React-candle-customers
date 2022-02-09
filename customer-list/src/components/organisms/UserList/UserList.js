dc8fc4477ff50724d3a968e0bce2873af65e5de1
Your Name <you@example.com>
2022-02-09 22:26:48 +0100

    adding navigation

--- customer-list/src/components/organisms/UserList/UserList.js
+++ customer-list/src/components/organisms/UserList/UserList.js
@@ -1,21 +1,27 @@
-import React, {useState} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import UserListItem from 'components/molecules/UserListItem/UserListItem';
-import { StyledList, StyledTitle, Wrapper } from './UserList.styles';
+import { StyledList } from './UsersList.styles';
+import { UserShape } from 'types';
+import { Title } from 'components/atoms/Title/Title';
 
 
 const UsersList = ({users, deleteUser}) => {
     return(
         <>
-            <Wrapper>
-            <StyledTitle>Customer List</StyledTitle>
+            <Title>Customer List</Title>
             <StyledList>
                 {users.map((userData) => (
                     <UserListItem deleteUser={deleteUser} key={userData.imie} userData={userData}/>
                 ))}
             </StyledList>
-        </Wrapper>
         </>  
 );
 };
+
+UsersList.propTypes = {
+    users: PropTypes.arrayOf(PropTypes.shape(UserShape)),
+    deleteUser: PropTypes.func,
+  };
 
 export default UserList;
--- customer-list/src/vievs/Root.js
+++ customer-list/src/vievs/Root.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
-import UsersList from 'components/organisms/UsersList/UsersList';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'assets/styles/GlobalStyle';
 import { theme } from 'assets/styles/theme';
 import { Wrapper } from './Root.styles';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Form from 'components/organisms/Form/Form';
 import { users as usersData } from 'data/users';
+import MainTemplate from 'components/templates/MainTemplate/MainTemplate';
+import AddUser from 'views/AddUser';
+import Dashboard from 'views/Dashboard';
 
 const initialFormState = {
   name: '',
@@ -47,11 +48,8 @@ const Root = () => {
     <Router>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        <MainTemplate>
         <Wrapper>
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/add-user">Add user</Link>
-          </nav>
           <Switch>
             <Route path="/add-user">
               <Form formValues={formValues} handleAddUser={handleAddUser} handleInputChange={handleInputChange} />
@@ -61,6 +59,7 @@ const Root = () => {
             </Route>
           </Switch>
         </Wrapper>
+        </MainTemplate>
       </ThemeProvider>
     </Router>
   );
